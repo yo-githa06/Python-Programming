105f2bb0de16b6daf67b538389ef8a6ea8c928fb
= <=>
2026-06-05 16:37:47 +0530

    Day1 in Python

---
+++ Day01.py
@@ -0,0 +1,30 @@
+print("Hello, World!")
+print("Hello All, Welcome to python")
+print("Welcome to coding with python")
+first_name = "Yogitha"# Variable declaration and assignment
+last_name = "Doodimadugula"
+print("Welcome " + first_name)#combining string with variable
+print(type(first_name))#to check the datatype of a variable
+Full_name = first_name + " " + last_name
+print(Full_name)
+age = 19
+age += 1
+Update_age = 12#This is snake case
+print(age + Update_age)
+name = "Yogitha"
+print (name + " is " + str(age) + " years old")#Str Concatenation in Python
+print(type(age))# Type method to check the datatype of a variable
+age += 1
+print(age)
+weight = 56.5
+print(name  + " is " + str(weight) + " kgs in weight")
+print(type(weight))
+is_pass = True
+print("Her result is " + " " +(str(is_pass)))
+print("Are u suffering with Overweight: " + " " + (str(is_pass)))
+print(type(is_pass))
+
+
+
+
+
